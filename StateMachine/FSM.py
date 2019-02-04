f88eecef5ac7b07d29c7b967d626c1328f0016ce
Matthew <matthew.c.r.millar@gmail.com>
2019-02-04 16:42:57 +0900

    Improved version to show case how to better control the flow of logic

--- StateMachine/FSM.py
+++ StateMachine/FSM.py
@@ -1,7 +1,35 @@
-import StateMachine.State as state
+import StateMachine.State as my_state
 
-print(state.fsm.current)
-state.fsm.sleep()
-print(state.fsm.current)
-state.fsm.wakeup()
-print(state.fsm.current)
+
+def askUser(state):
+    task = input("What should I do")
+
+    if task == "wake":
+        if state.fsm.current == "awake":
+            print("All ready awake")
+        else:
+            state.fsm.wakeup()
+        print(state.fsm.current)
+        askUser(state)
+    elif task == "sleep":
+        if state.fsm.current == "sleeping":
+            print("All ready sleeping")
+        else:
+            state.fsm.sleep()
+        print(state.fsm.current)
+        askUser(state)
+    else:
+        pass
+
+
+
+print("Starting")
+
+askUser(my_state)
+print("Ending")
+
+
+
+
+
+
