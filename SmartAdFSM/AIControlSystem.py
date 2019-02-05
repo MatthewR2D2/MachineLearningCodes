2848b10ba9b1de00082daf96f469c03ded4d8bca
Matthew <matthew.c.r.millar@gmail.com>
2019-02-05 15:52:44 +0900

    AI control mechanism for controlling the FSM

---
+++ SmartAdFSM/AIControlSystem.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+'''
+#This is the AI controller systems that controls the FSM states and changes.
+# this is just to control IdeaBit's ads (Google Adwords, YahooAds, FacebookAds)
+# This will NOT control their customers ads and manage them.
+
+# This is the main controller for each FSM Ad system. All changes to the FSM will be done through here.
+__author__ = "Matthew Millar"
+__copyright__ = ""
+__credits__ =
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = "Matthew Millar"
+__email__ = "matthew.millar@igniterlabs.com"
+__status__ = "Dev"
+
+'''
+
+from SmartAdFSM import FiniteStateMachine as fsm
+
+
+# This is a test controller that takes in a machine and a trigger then set the machine state correctly
+def controlFSM(machine, trigger):
+    if trigger == "Start":
+        if machine.current == "start":
+            print('Ad is all ready running')
+        else:
+            print("You cannot start a ad that has already started")
+    elif trigger == "End":
+        if machine.current == "endAd":
+            print("Ad is all ready ended")
+        else:
+            print("Ad is ending")
+            machine.end()
+    elif trigger == "activate":
+        if machine.current == "activeAd":
+            print("Ad is all ready active")
+        else:
+            machine.activate()
+            print("Ad Activated")
+    elif trigger == "deactivate":
+        if machine.current == "inactiveAd":
+            print("Ad is already inactive")
+        else:
+            machine.deactivate()
+            print("Ad is now inactive")
+
+
+def printStateOfEachMachine(name, machine):
+    print('FSM: {} is at state {}'.format(name, machine.current))
+
+
+'''
+# this is an example of how to use the FMS and AI controller together 
+'''
+if __name__ == '__main__':
+    print("Creating Yahoo ad controler")
+    # Create a FSM for yahoo ad and google ad
+    yahooMachine = fsm.createFSM()
+    googleMachine = fsm.createFSM()
+    facebookMachine = fsm.createFSM()
+
+    printStateOfEachMachine("Yahoo", yahooMachine)
+    printStateOfEachMachine("Google", googleMachine)
+    printStateOfEachMachine("Facebook", facebookMachine)
+
+    activateTrigger = "activate"
+    deactivateTrigger = "deactivate"
+    endTrigger = "End"
+    startTrigger = "Start"
+
+    print("Starting Simple Test")
+    controlFSM(yahooMachine, startTrigger)
+    controlFSM(yahooMachine, activateTrigger)
+    controlFSM(yahooMachine, activateTrigger)
+    controlFSM(yahooMachine, deactivateTrigger)
+    controlFSM(yahooMachine, deactivateTrigger)
+    controlFSM(yahooMachine, activateTrigger)
+    controlFSM(yahooMachine, startTrigger)
+    controlFSM(yahooMachine, endTrigger)
+    print("Simple Test Finished Successfully")
+
+    controlFSM(googleMachine, activateTrigger)
+
+    controlFSM(facebookMachine, deactivateTrigger)
+
+    # Create a FSM for yahoo ad
+    printStateOfEachMachine("Yahoo", yahooMachine)
+    printStateOfEachMachine("Google", googleMachine)
+    printStateOfEachMachine("Facebook", facebookMachine)
