63b503562d1e5e48015bd71edecb8aacc64fd1ef
Matthew <matthew.c.r.millar@gmail.com>
2019-02-06 12:42:36 +0900

    Updated Unit test for changes in yahooFSM

--- SmartAdFSM/UnitTest/FiniteStateMachineUnitTest.py
+++ SmartAdFSM/UnitTest/FiniteStateMachineUnitTest.py
@@ -17,7 +17,7 @@ __status__ = "Dev"
 '''
 
 import unittest
-from SmartAdFSM import FiniteStateMachine as fsm
+from SmartAdFSM import YahooFiniteStateMachine as fsm
 
 class FSMTest(unittest.TestCase):
 
@@ -27,52 +27,52 @@ class FSMTest(unittest.TestCase):
     # Test the activte function from start active inactive end
     def testSimpleFlow(self):
         # check to see if the current state is start
-        self.assertEqual("start", self.myMachine.current)
+        self.assertEqual("START", self.myMachine.current)
         # Change state to active
         self.myMachine.activate()
-        self.assertEqual("activeAd", self.myMachine.current)
+        self.assertEqual("ACTIVE", self.myMachine.current)
         # Change state to inactive
         self.myMachine.deactivate()
-        self.assertEqual("inactiveAd", self.myMachine.current)
+        self.assertEqual("PAUSED", self.myMachine.current)
         # Change state to end ad
         self.myMachine.end()
-        self.assertEqual("endAd", self.myMachine.current)
+        self.assertEqual("DELETED", self.myMachine.current)
 
     def testStartToEnd(self):
         # Start with start state then go directly to end state
-        self.assertEqual("start", self.myMachine.current)
+        self.assertEqual("START", self.myMachine.current)
         # Change state to end ad
         self.myMachine.end()
-        self.assertEqual("endAd", self.myMachine.current)
+        self.assertEqual("DELETED", self.myMachine.current)
 
     def testStartToInactive(self):
         # check to see if the current state is start
-        self.assertEqual("start", self.myMachine.current)
+        self.assertEqual("START", self.myMachine.current)
         # Change state to inactive
         self.myMachine.deactivate()
-        self.assertEqual("inactiveAd", self.myMachine.current)
+        self.assertEqual("PAUSED", self.myMachine.current)
 
     def testSwitchBetweenActiveInactive(self):
         # check to see if the current state is start
-        self.assertEqual("start", self.myMachine.current)
+        self.assertEqual("START", self.myMachine.current)
         # Change state to active
         self.myMachine.activate()
-        self.assertEqual("activeAd", self.myMachine.current)
+        self.assertEqual("ACTIVE", self.myMachine.current)
         # Change state to inactive
         self.myMachine.deactivate()
-        self.assertEqual("inactiveAd", self.myMachine.current)
+        self.assertEqual("PAUSED", self.myMachine.current)
         # Change state to active
         self.myMachine.activate()
-        self.assertEqual("activeAd", self.myMachine.current)
+        self.assertEqual("ACTIVE", self.myMachine.current)
         # Change state to inactive
         self.myMachine.deactivate()
-        self.assertEqual("inactiveAd", self.myMachine.current)
+        self.assertEqual("PAUSED", self.myMachine.current)
         # Change state to active
         self.myMachine.activate()
-        self.assertEqual("activeAd", self.myMachine.current)
+        self.assertEqual("ACTIVE", self.myMachine.current)
         # Change state to inactive
         self.myMachine.deactivate()
-        self.assertEqual("inactiveAd", self.myMachine.current)
+        self.assertEqual("PAUSED", self.myMachine.current)
 
 
 
