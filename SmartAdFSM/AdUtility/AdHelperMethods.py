0669de761cfdcc1b71a9f92bf6470de6a8418751
Matthew <matthew.c.r.millar@gmail.com>
2019-02-12 14:51:53 +0900

    Clean up and added comments

--- SmartAdFSM/AdUtility/AdHelperMethods.py
+++ SmartAdFSM/AdUtility/AdHelperMethods.py
@@ -18,9 +18,16 @@ __status__ = "Dev"
 
 from SmartAdFSM.AdUtility import YahooJsonParser as yjp
 
+'''
+This method is for reading in a json from a file or a URL
+@:param readFromFile - boolean tell which method to read from file/url
+@:param myjson - this is the json source file/url
+@:param yahooAdList - this holds all the ads from yahoo
+@:param currentAds - This holds every ad that is from all service providers.
+'''
+
 
 def CreateYahooAdList(readFromFile, myjson, yahooAdList, currentAds):
-
     # Clear all arrays Do this to make sure array is cleared
     currentAds.clear()
     yahooAdList.clear()
@@ -54,11 +61,17 @@ def UpdateUIValues(dropdownValues, currentAds):
     # clear array that holds list of values for the dropdown box
     dropdownValues.clear()
     for ad in currentAds:
-        apiHost = ad[0]        # Where the ad is hosted
-        title = ad[1].title    # The title from the ad
+        apiHost = ad[0]  # Where the ad is hosted
+        title = ad[1].title  # The title from the ad
         status = ad[1].status  # The curent status of the ad
         dropdownValues.append(apiHost + ":" + title + ':' + status)  # Create the list string for the drop down menu
 
+
+'''
+This method is to update the UI for the dropdownWidget
+@:param dropdownWidget - is the dropdown menu that displays the currently available ads
+@:param dropdownValues - is the list of available ads from every vendor
+'''
 
 
 def UpdateUI(dropdownWidget, dropdownValues):
