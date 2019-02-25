0f0d42e3ef7cf41f97e691f2c6608e27bb2d7d9e
Matthew <matthew.c.r.millar@gmail.com>
2019-02-25 16:51:45 +0900

    CNN and Linear regression model combined into one large predictive model

---
+++ DNN/MultipleModelsCombined/HouseDataset/MixedModel.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+'''
+# Short Description@
+# 
+# Full Description@
+# 
+__author__ = "Matthew Millar"
+__copyright__ = ""
+__credits__ =
+__license__ = ""
+__version__ = "0.0.0"
+__maintainer__ = "Matthew Millar"
+__email__ = "matthew.millar@igniterlabs.com"
+__status__ = "Dev"
+
+'''
+
+# import the necessary packages
+
+from MixedModelUtils import DatasetsHelper
+from MixedModelUtils import models
+from sklearn.model_selection import train_test_split
+from keras.layers.core import Dense
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import concatenate
+import numpy as np
+import locale
+
+print("Loading house attributes")
+
+# Load the data set for the houses text file
+inputpath = "HousesDataset/HousesInfo.txt"
+df = DatasetsHelper.loadHouseAttributes(inputpath)
+print(df.head())
+
+imagePath = "HousesDataset/"
+images = DatasetsHelper.loadHouseImages(df, imagePath)
+images = images / 255.0
+
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+split = train_test_split(df, images, test_size=0.25, random_state=42)
+(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+
+# find the largest house price in the training set and use it to
+# scale our house prices to the range [0, 1] (will lead to better
+# training and convergence)
+maxPrice = trainAttrX["price"].max()
+trainY = trainAttrX["price"] / maxPrice
+testY = testAttrX["price"] / maxPrice
+
+# process the house attributes data by performing min-max scaling
+# on continuous features, one-hot encoding on categorical features,
+# and then finally concatenating them together
+(trainAttrX, testAttrX) = DatasetsHelper.process_house_attributes(df,
+                                                                  trainAttrX, testAttrX)
+
+# create the MLP and CNN models
+mlp = models.create_mlp(trainAttrX.shape[1], regress=False)
+cnn = models.create_cnn(64, 64, 3, regress=False)
+
+# create the input to our final set of layers as the *output* of both
+# the MLP and CNN
+combinedInput = concatenate([mlp.output, cnn.output])
+
+# our final FC layer head will have two dense layers, the final one
+# being our regression head
+x = Dense(4, activation="relu")(combinedInput)
+x = Dense(1, activation="linear")(x)
+
+# our final model will accept categorical/numerical data on the MLP
+# input and images on the CNN input, outputting a single value (the
+# predicted price of the house)
+model = Model(inputs=[mlp.input, cnn.input], outputs=x)
+
+# compile the model using mean absolute percentage error as our loss,
+# implying that we seek to minimize the absolute percentage difference
+# between our price *predictions* and the *actual prices*
+opt = Adam(lr=1e-3, decay=1e-3 / 200)
+model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+
+# train the model
+print("training model...")
+model.fit(
+    [trainAttrX, trainImagesX], trainY,
+    validation_data=([testAttrX, testImagesX], testY),
+    epochs=200, batch_size=8)
+
+# make predictions on the testing data
+print("predicting house prices...")
+preds = model.predict([testAttrX, testImagesX])
+
+# compute the difference between the *predicted* house prices and the
+# *actual* house prices, then compute the percentage difference and
+# the absolute percentage difference
+diff = preds.flatten() - testY
+percentDiff = (diff / testY) * 100
+absPercentDiff = np.abs(percentDiff)
+
+# compute the mean and standard deviation of the absolute percentage difference
+mean = np.mean(absPercentDiff)
+std = np.std(absPercentDiff)
+
+# output results
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+print("[INFO] avg. house price: {}, std house price: {}".format(
+    locale.currency(df["price"].mean(), grouping=True),
+    locale.currency(df["price"].std(), grouping=True)))
+print("[INFO] mean: {:.2f}%, std: {:.2f}%".format(mean, std))
