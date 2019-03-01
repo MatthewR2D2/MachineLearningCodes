46a2fd6fd6cee0baa5ef4fc4dd824fbf8bf5e062
Matthew <matthew.c.r.millar@gmail.com>
2019-03-01 17:23:44 +0900

    VGGNet Example

---
+++ DNN/SelfDrivingCarTrafficSign/TrafficSignClassification/HelperMethods/VisulizationMethods.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+'''
+# Short Description@
+# 
+# Full Description@
+# 
+__author__ = "Matthew Millar"
+__copyright__ = ""
+__credits__ =
+__license__ = ""
+__version__ = "0.0.0"
+__maintainer__ = "Matthew Millar"
+__email__ = "matthew.millar@igniterlabs.com"
+__status__ = "Dev"
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import skimage.morphology as morp
+from skimage.filters import rank
+
+def list_images(signs, dataset, dataset_y, ylabel="", cmap=None):
+    """
+    Display a list of images in a single figure with matplotlib.
+        Parameters:
+            images: An np.array compatible with plt.imshow.
+            lanel (Default = No label): A string to be used as a label for each image.
+            cmap (Default = None): Used to display gray images.
+    """
+    plt.figure(figsize=(15, 16))
+    for i in range(6):
+        plt.subplot(1, 6, i+1)
+        indx = random.randint(0, len(dataset))
+        #Use gray scale color map if there is only one channel
+        cmap = 'gray' if len(dataset[indx].shape) == 2 else cmap
+        plt.imshow(dataset[indx], cmap = cmap)
+        plt.xlabel(signs[dataset_y[indx]])
+        plt.ylabel(ylabel)
+        plt.xticks([])
+        plt.yticks([])
+    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
+    plt.show()
+
+
+
+def histogram_plot(n_classes, dataset, label):
+    """
+    Plots a histogram of the input data.
+        Parameters:
+            dataset: Input data to be plotted as a histogram.
+            lanel: A string to be used as a label for the histogram.
+    """
+    hist, bins = np.histogram(dataset, bins=n_classes)
+    width = 0.7 * (bins[1] - bins[0])
+    center = (bins[:-1] + bins[1:]) / 2
+    plt.bar(center, hist, align='center', width=width)
+    plt.xlabel(label)
+    plt.ylabel("Image count")
+    plt.show()
+
+def local_histo_equalize(image):
+    """
+    Apply local histogram equalization to grayscale images.
+        Parameters:
+            image: A grayscale image.
+    """
+    kernel = morp.disk(30)
+    img_local = rank.equalize(image, selem=kernel)
+    return img_local
