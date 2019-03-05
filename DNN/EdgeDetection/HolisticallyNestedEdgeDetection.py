617834434256eb695c9a394f0d642b3508d33551
Matthew <matthew.c.r.millar@gmail.com>
2019-03-05 14:40:48 +0900

    Tested for TRex

--- DNN/EdgeDetection/HolisticallyNestedEdgeDetection.py
+++ DNN/EdgeDetection/HolisticallyNestedEdgeDetection.py
@@ -18,11 +18,13 @@ __status__ = "Dev"
 
 import cv2
 from HelperClasses.CropLayer import CropLayer
+import datetime
+import time
 
 # Paths to models and data
 protoPath = "/home/matt/GitClone/PythonGitRepo/DNN/EdgeDetection/hed_model/deploy.prototxt"
 modelPath = "/home/matt/GitClone/PythonGitRepo/DNN/EdgeDetection/hed_model/hed_pretrained_bsds.caffemodel"
-imagePath = "/home/matt/GitClone/PythonGitRepo/DNN/EdgeDetection/TestImages/pen.jpg"
+imagePath = "/home/matt/GitClone/PythonGitRepo/DNN/EdgeDetection/TestImages/trex.png"
 
 net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
@@ -30,6 +32,8 @@ net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 cv2.dnn_registerLayer("Crop", CropLayer)
 
 # Load image
+
+start = time.gmtime()
 image = cv2.imread(imagePath)
 # Get the height and width of the image
 (h,w) = image.shape[:2]
@@ -46,6 +50,8 @@ hed = net.forward()
 hed = cv2.resize(hed[0,0], (w,h)) # Resize the new image to the original size
 hed = (255 * hed).astype("uint8") # Scale pixels back to range of [0, 255] Make sure its type uint8
 
+stop = time.gmtime()
+print()
 # Print out the original image and the new HED image
 cv2.imshow("Original", image)
 cv2.imshow("HED", hed)
